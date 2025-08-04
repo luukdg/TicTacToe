0c6e0112a638a3fc52fe03d7d6b0f6907680b70c
luukdg <luukdegraaf1993@gmail.com>
2025-08-04 15:07:55 +0200

    feat: implement game field interaction and styling for Tic Tac Toe

--- main.js
+++ main.js
@@ -6,22 +6,40 @@ let gameBoard = {
     row3: ["", "", ""]
 }
 
+let playerOrder = 1
+
 const gameLoop = (gameBoard) => {
-    if (gameBoard.row1[0]) {
-        console.log("Choose again.")
-    } else {
-    gameBoard.row1[0] = "X";
-    console.log(gameBoard);
-    gameStatus(gameBoard);
-    }
-
-    gameBoard.row2[1] = "X";
-    console.log(gameBoard);
-    gameStatus(gameBoard);
-
-    gameBoard.row3[2] = "X";
-    console.log(gameBoard);
-    gameStatus(gameBoard);
+    
 }
 
-gameLoop(gameBoard);
+const gameField = () => {
+    // Document querySelectors
+    const playField = document.querySelectorAll("#id-1, #id-2, #id-3, #id-4, #id-5, #id-6, #id-7, #id-8, #id-9");
+
+    const A1Dom = playField[0]
+
+    let A1 = gameBoard.row1[0]
+
+    // Field 1
+    A1Dom.addEventListener("click", () => {
+        if (gameBoard.row1[0]) {
+            console.log("Choose again.")
+            return;
+        } else if (playerOrder === 1) {
+            A1 = "X";
+            A1Dom.textContent="X";
+            console.log(gameBoard);
+            gameStatus(gameBoard);
+            playerOrder = 2;
+        } else {
+            A1 = "O";
+            A1Dom.textContent="O";
+            console.log(gameBoard);
+            gameStatus(gameBoard);
+            playerOrder = 1;
+        }
+    })
+}
+
+gameField();
+// gameLoop(gameBoard);
